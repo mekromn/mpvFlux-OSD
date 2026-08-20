a528d6aafebc2fb9702e725e3db38c8a6d56c952
mekromn <67240529+mekromn@users.noreply.github.com>
2026-08-20 12:34:31 -0700

    R04 register Shader Lab engine installer

--- app/src/main/java/app/marlboroadvance/mpvex/di/ShaderLabModule.kt
+++ app/src/main/java/app/marlboroadvance/mpvex/di/ShaderLabModule.kt
@@ -1,9 +1,11 @@
 package app.marlboroadvance.mpvex.di
 
+import app.marlboroadvance.mpvex.repository.shaderlab.ShaderLabEngineInstaller
 import app.marlboroadvance.mpvex.repository.shaderlab.ShaderLabWorkspaceManager
 import org.koin.dsl.module
 
 val ShaderLabModule =
   module {
     single { ShaderLabWorkspaceManager(get()) }
+    single { ShaderLabEngineInstaller(context = get(), workspaceManager = get()) }
   }
